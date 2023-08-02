6f49e856538e0966707dcc28141c792947edd8af
ziwanarp <ziwanarizwan@gmail.com>
2023-08-02 22:08:37 +0700

    initial commit & create migration

---
+++ database/migrations/2023_08_02_145949_create_addresses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('customer_id');
+            $table->string('address');
+            $table->string('district');
+            $table->string('city');
+            $table->string('province');
+            $table->integer('postal_code');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+};
