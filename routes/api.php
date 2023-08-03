52243b0f8e7387717cb94b7cf87461a85d655f6f
Ziwana <ziwana@newtronic-solution.com>
2023-08-03 13:32:17 +0700

    add routes apiResource

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AddressesController;
+use App\Http\Controllers\CustomersController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::apiResource('customer', CustomersController::class)->except('create','edit');
+Route::apiResource('address', AddressesController::class);
