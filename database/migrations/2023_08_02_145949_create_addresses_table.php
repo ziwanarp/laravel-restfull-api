fc52dd4f18c8c6b51c24f72e81419e81b3c765ff
Ziwana <ziwana@newtronic-solution.com>
2023-08-03 13:29:54 +0700

    update database migration

--- database/migrations/2023_08_02_145949_create_addresses_table.php
+++ database/migrations/2023_08_02_145949_create_addresses_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('customer_id');
+            $table->integer('customer_id');
             $table->string('address');
             $table->string('district');
             $table->string('city');
