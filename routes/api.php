5ba6a8bd3d6a6f90e1c920db7b6b2dcbc20db058
ziwanarp <ziwanarizwan@gmail.com>
2023-08-03 23:19:03 +0700

    Update route

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::apiResource('customer', CustomersController::class)->except('create','edit');
-Route::apiResource('address', AddressesController::class);
+Route::apiResource('address', AddressesController::class)->except('create','edit');
