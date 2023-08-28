f2d33cd76c13335dd1f282acc680b2868f324eeb
Ziwana <ziwana@newtronic-solution.com>
2023-08-28 14:47:13 +0700

    update controller

--- app/Http/Controllers/AddressesController.php
+++ app/Http/Controllers/AddressesController.php
@@ -2,158 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Addresses;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Services\AddressService;
 
 class AddressesController extends Controller
 {
+    private $address;
+
+    public function __construct()
+    {
+        $this->address = new AddressService;
+    }
     
     public function index()
     {
-         // get all address
-         $address = Addresses::all();
-
-         // condition if address 0 or !0
-         if(count($address) != 0){
-             return response()->json([
-                 'status' => 200,
-                 'message' =>'success',
-                 'data' => $address,
-             ],200);
-         } else {
-             return response()->json([
-                 'status' => 200,
-                 'message'=>'no data available',
-                 'data' => $address,
-             ],200);
-         }
+         $this->address->getAll();
     }
 
-    
     public function store(Request $request)
     {
-        // set rules
-        $rules = [
-            'customer_id' => 'required|integer',
-            'address' => 'required|max:255',
-            'district' => 'required|max:255',
-            'city' => 'required|max:255',
-            'province' => 'required|max:255',
-            'postal_code' => 'required|integer'
-        ];
-
-        // Set validation
-        $validator = Validator::make($request->all(), $rules);
-
-        // if validate error return 422
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 422,
-                'message' => $validator->messages()->first()
-            ],422);
-       }
-
-        //insert data
-        $address = Addresses::create($request->all());
-
-        // return json response 201
-        return response()->json([
-            'status' => 201,
-            'message' => 'data created',
-            'data' => $address
-        ],201);
+        $this->address->addAddress($request);
     }
 
-    
     public function show($address)
     {
-        // find address by id
-        $addresses = Addresses::find($address);
-        
-        // condition if address 0 or !0
-        if($addresses != null){
-            return response()->json([
-                'status'=> 200,
-                'message' => 'success',
-                'data' => $addresses,
-            ],200);
-        } else {
-            return response()->json([
-                'status'=> 200,
-                'message' => 'no data available',
-                'data' => $addresses,
-            ],200);
-        }
+        return $this->address->getAddress($address);
     }
 
-    
     public function update(Request $request, $address)
     {
-        // find address by id
-        $addresses = Addresses::find($address);
-
-        if($addresses != null){
-            // set rules validation
-            $rules = [
-                'customer_id' => 'required|integer',
-                'address' => 'required|max:255',
-                'district' => 'required|max:255',
-                'city' => 'required|max:255',
-                'province' => 'required|max:255',
-                'postal_code' => 'required|integer'
-            ];
-            // set validation
-            $validator = Validator::make($request->all(), $rules);
-
-            // if validate error return 422
-            if ($validator->fails()) {
-                return response()->json([
-                    'status' => 422,
-                    'message' => $validator->messages()->first()
-                ],422);
-            }
-            // set data for update
-            $addresses->customer_id = $request->customer_id;
-            $addresses->address = $request->address;
-            $addresses->district = $request->district;
-            $addresses->city = $request->city;
-            $addresses->province = $request->province;
-            $addresses->postal_code = $request->postal_code;
-            $addresses->save();
-
-            return response()->json([
-                'status' => 200,
-                'message' => 'data updated',
-                'data' => $addresses,
-            ],200);
-        } else {
-            return response()->json([
-                'status' => 200,
-                'message' => 'update failed',
-                'data' => $addresses,
-            ],200);
-
-        }
+        return $this->address->updateAddress($request, $address);
     }
 
-    
     public function destroy($address)
     {
-        // find address and delete by id
-        $deleted = Addresses::destroy($address);
-
-        if($deleted != 0){
-            return response()->json([
-                'status' => 200,
-                'message' => 'success deleted',
-            ],200);
-        } else {
-            return response()->json([
-                'status' => 200,
-                'message' => 'delete failed',
-            ],200);
-
-        }
+       return $this->address->deleteAddress($address);
     }
 }
--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -2,176 +2,40 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Addresses;
-use App\Models\Customers;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
+use App\Services\CustomerService;
 
 class CustomersController extends Controller
 {
-    
-    public function index()
-    {
-        // get all customers
-        $customer = Customers::all();
+    private $customer;
 
-        // condition if customers 0 or !0
-        if(count($customer) != 0){
-            return response()->json([
-                'status' => 200,
-                'message' =>'success',
-                'data' => $customer,
-            ],200);
-        } else {
-            return response()->json([
-                'status' => 200,
-                'message'=>'no data available',
-                'data' => $customer,
-            ],200);
-        }
+    public function __construct()
+    {
+        $this->customer = new CustomerService;
     }
 
+    public function index()
+    {
+        return $this->customer->getAll();
+    }
 
     public function store(Request $request)
     {
-        // set rules
-        $rules = [
-            'title' => 'required|in:Mr,Mrs',
-            'name' => 'required|max:255',
-            'gender' => 'required|in:M,F',
-            'phone_number' => 'required|min:10|max:13|unique:customers',
-            'image' => 'required|max:255',
-            'email' => 'required|email|unique:customers|max:255'
-        ];
-
-        // Set validation
-        $validator = Validator::make($request->all(), $rules);
-
-        // if validate error return 422
-        if ($validator->fails()) {
-            return response()->json([
-                'status' => 422,
-                'message' => $validator->messages()->first()
-            ],422);
-       }
-
-        //insert data
-        $customer = Customers::create($request->all());
-
-        // return json response 201
-        return response()->json([
-            'status' => 201,
-            'message' => 'data created',
-            'data' => $customer
-        ],201);
-        
+       return $this->customer->addCustomer($request);
     }
-
 
     public function show($customer)
     {
-        // find customer by id
-        $customers = Customers::find($customer);
-        $addresses = Addresses::where('customer_id', $customer)->get();
-        
-        // condition if customer 0 or !0
-        if($customers != null){
-            $customers['address'] = $addresses;
-
-            return response()->json([
-                'status'=> 200,
-                'message' => 'success',
-                'data' => $customers,
-            ],200);
-        } else {
-            return response()->json([
-                'status'=> 200,
-                'message' => 'no data available',
-                'data' => $customers,
-            ],200);
-        }
-        
+        return $this->customer->getCustomer($customer);
     }
-
 
     public function update(Request $request, $customer)
     {
-        $customers = Customers::find($customer);
-
-        if($customers != null){
-            // set rules validation
-            $rules = [
-                'title' => 'required|in:Mr,Mrs',
-                'name' => 'required|max:255',
-                'gender' => 'required|in:M,F',
-                'image' => 'required|max:255',
-            ];
-
-            if ($request->email != $customers->email) {
-                $rules['email'] = 'required|email|unique:customers|max:255';
-            }
-
-            if ($request->phone_number != $customers->phone_number) {
-                $rules['phone_number'] = 'required|min:10|max:13|unique:customers';
-            }
-
-            $validator = Validator::make($request->all(), $rules);
-
-            // if validate error return 422
-            if ($validator->fails()) {
-                return response()->json([
-                    'status' => 422,
-                    'message' => $validator->messages()->first()
-                ],422);
-            }
-
-            // Customers::where('id', $customer)->update([$request->all()]);
-            
-            $customers->title = $request->title;
-            $customers->name = $request->name;
-            $customers->gender = $request->gender;
-            $customers->phone_number = $request->phone_number;
-            $customers->image = $request->image;
-            $customers->email = $request->email;
-            $customers->save();
-
-            return response()->json([
-                'status' => 200,
-                'message' => 'data updated',
-                'data' => $customers,
-            ],200);
-        } else {
-            return response()->json([
-                'status' => 200,
-                'message' => 'update failed',
-                'data' => $customers,
-            ],200);
-
-        }
-
-        
+        return $this->customer->updateCustomer($request, $customer);
     }
-
 
     public function destroy($customer)
     {
-        // delete customer by id
-        $deleted = Customers::destroy($customer);
-        // delete address by customer_id
-        Addresses::where('customer_id', $customer)->delete();
-
-        if($deleted != 0){
-            return response()->json([
-                'status' => 200,
-                'message' => 'success deleted',
-            ],200);
-        } else {
-            return response()->json([
-                'status' => 200,
-                'message' => 'delete failed',
-            ],200);
-
-        }
-
+        return $this->customer->deleteCustomer($customer);
     }
 }
